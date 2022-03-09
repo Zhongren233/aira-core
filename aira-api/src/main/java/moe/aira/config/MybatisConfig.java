3b272ae6c5e0597cf2e08c9e11729b7d865b3f8b
ZhongRen <zhongren233@gmail.com>
2022-03-09 15:29:02 +0800

    1

--- aira-api/src/main/java/moe/aira/config/MybatisConfig.java
+++ aira-api/src/main/java/moe/aira/config/MybatisConfig.java
@@ -3,14 +3,18 @@ package moe.aira.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
+@Slf4j
 public class MybatisConfig {
     @Value(
             "${aira.core.async.dao-poolSize}"
@@ -27,6 +31,16 @@ public class MybatisConfig {
 
     @Bean("daoAsyncExecutor")
     public Executor daoAsyncExecutor() {
-        return Executors.newFixedThreadPool(threadPoolSize);
+        return Executors.newFixedThreadPool(threadPoolSize, new ThreadFactory() {
+            private final AtomicInteger atomicInteger = new AtomicInteger(1);
+
+            @Override
+            public Thread newThread(Runnable r) {
+                int andAdd = atomicInteger.getAndAdd(1);
+                String name = "dao-async" + andAdd;
+                log.info("daoAsyncExecutor:create {}", name);
+                return new Thread(r, name);
+            }
+        });
     }
 }
